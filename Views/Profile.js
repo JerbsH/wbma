d75550331bc5f25764c90f40bb97862ed81d63d6
Jere <101744771+JerbsH@users.noreply.github.com>
2023-09-01 11:01:39 +0300

    dummy login created

--- Views/Profile.js
+++ Views/Profile.js
@@ -1,11 +1,24 @@
-import React from 'react';
-import {Platform, SafeAreaView, StyleSheet, Text} from 'react-native';
+import React, {useContext} from 'react';
+import {Button, Platform, SafeAreaView, StyleSheet, Text} from 'react-native';
+import {MainContext} from '../contexts/MainContext';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Profile = (props) => {
+  const {setIsLoggedIn} = useContext(MainContext);
+  const logOut = async () => {
+    console.log('profile, logout');
+    try {
+      await AsyncStorage.clear();
+      setIsLoggedIn(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <>
       <SafeAreaView style={styles.container}>
         <Text>Profile view</Text>
+        <Button title="Log out!" onPress={logOut}></Button>
       </SafeAreaView>
     </>
   );
