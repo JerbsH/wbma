f4eed43ca24ae6055fa952fade15b86018a136e9
Jere <101744771+JerbsH@users.noreply.github.com>
2023-09-10 22:49:01 +0300

    icons

--- Views/Home.js
+++ Views/Home.js
@@ -6,42 +6,12 @@ import PropTypes from 'prop-types';
 const Home = ({navigation}) => {
   return (
     <>
-      <SafeAreaView style={styles.container}>
+      <SafeAreaView>
         <List navigation={navigation} />
       </SafeAreaView>
     </>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    paddingTop: Platform.OS === 'android' ? 30 : 0,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  TouchableOpacity: {
-    marginVertical: 2,
-  },
-  box: {
-    flex: 1,
-    flexDirection: 'row',
-    backgroundColor: 'lightgrey',
-  },
-  Image: {
-    margin: 5,
-    width: '45%',
-    height: 'auto',
-  },
-  View: {
-    width: '50%',
-    margin: 5,
-  },
-  Text: {
-    height: 'auto',
-    width: '100%',
-  },
-});
 
 Home.propTypes = {
   navigation: PropTypes.object,
