199e892dac62388fb4894d06b46b1e5cff693a13
Jere <101744771+JerbsH@users.noreply.github.com>
2023-09-18 13:29:54 +0300

    player to single added

--- Views/Single.js
+++ Views/Single.js
@@ -1,34 +1,55 @@
+/* eslint-disable camelcase */
 import React from 'react';
 import {Text} from 'react-native';
 import {PropTypes} from 'prop-types';
 import {mediaUrl} from '../utils/app-config';
 import {Card, Icon, Image, ListItem} from '@rneui/themed';
+import {Video} from 'expo-av';
 
 const Single = ({route, navigation}) => {
-  const singleMedia = route.params;
+  const {
+    title,
+    description,
+    filename,
+    time_added: timeAdded,
+    user_id: userId,
+    media_type,
+  } = route.params;
   return (
     <Card containerStyle={{width: '100%', margin: 'auto'}}>
-      <Image
-        source={{
-          uri: mediaUrl + singleMedia.filename,
-        }}
-        containerStyle={{width: '100%', height: 325}}
-      ></Image>
+      {media_type === 'image' ? (
+        <Image
+          source={{
+            uri: mediaUrl + filename,
+          }}
+          containerStyle={{width: '100%', height: 325}}
+        ></Image>
+      ) : (
+        <Video
+          source={{uri: mediaUrl + filename}}
+          style={{height: 300}}
+          useNativeControls
+          resizeMode="contain"
+          shouldPlay={true}
+          isLooping
+        ></Video>
+      )}
+
       <ListItem>
         <Icon name="chat"></Icon>
-        <Text>{singleMedia.title}</Text>
+        <Text>{title}</Text>
       </ListItem>
       <ListItem>
         <Icon name="list"></Icon>
-        <Text>Description: {singleMedia.description}</Text>
+        <Text>Description: {description}</Text>
       </ListItem>
       <ListItem>
         <Icon name="event"></Icon>
-        <Text>Uploaded: {singleMedia.time_added.slice(0, -14)}</Text>
+        <Text>Uploaded: {timeAdded.slice(0, -14)}</Text>
       </ListItem>
       <ListItem>
         <Icon name="person"></Icon>
-        <Text>{singleMedia.user_id}</Text>
+        <Text>{userId}</Text>
       </ListItem>
     </Card>
   );
