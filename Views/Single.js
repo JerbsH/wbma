c956312924e4a62c8adea34c1307084aa4fc09db
Jere <101744771+JerbsH@users.noreply.github.com>
2023-09-18 15:07:13 +0300

    added liking functionality

--- Views/Single.js
+++ Views/Single.js
@@ -1,23 +1,86 @@
-/* eslint-disable camelcase */
-import React from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {Text} from 'react-native';
 import {PropTypes} from 'prop-types';
 import {mediaUrl} from '../utils/app-config';
-import {Card, Icon, Image, ListItem} from '@rneui/themed';
+import {Button, Card, Icon, Image, ListItem} from '@rneui/themed';
 import {Video} from 'expo-av';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useFavourite, useUser} from '../hook/ApiHooks';
+import {MainContext} from '../contexts/MainContext';
 
 const Single = ({route, navigation}) => {
+  const [owner, setOwner] = useState({});
+  const [userLike, setUserLike] = useState(false);
+  const [likes, setLikes] = useState([]);
+  const {user} = useContext(MainContext);
+  const {getUserById} = useUser();
+  const {postFavourite, getFavouritesById, deleteFavourite} = useFavourite();
+
   const {
     title,
     description,
     filename,
     time_added: timeAdded,
     user_id: userId,
-    media_type,
+    media_type: mediaType,
+    file_id: fileId,
   } = route.params;
+
+  // fetch owner info
+  const fetchOwner = async () => {
+    try {
+      const token = await AsyncStorage.getItem('userToken');
+      const ownerData = await getUserById(userId, token);
+      setOwner(ownerData);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  // add favourite
+  const createFavourite = async () => {
+    try {
+      const token = await AsyncStorage.getItem('userToken');
+      const response = await postFavourite({file_id: fileId}, token);
+      response && setUserLike(true);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+  // delete favourite
+  const removeFavourite = async () => {
+    try {
+      const token = await AsyncStorage.getItem('userToken');
+      const response = await deleteFavourite(fileId, token);
+      response && setUserLike(false);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  // getfavouritesbyid
+  const fetchLikes = async () => {
+    try {
+      const likesData = await getFavouritesById(fileId);
+      setLikes(likesData);
+      // check if userid stored in context is in likesData
+      likesData.forEach((like) => {
+        if (like.user_id === user.user_id) {
+          setUserLike(true);
+        }
+      });
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchOwner();
+    fetchLikes();
+  }, []);
   return (
     <Card containerStyle={{width: '100%', margin: 'auto'}}>
-      {media_type === 'image' ? (
+      {mediaType === 'image' ? (
         <Image
           source={{
             uri: mediaUrl + filename,
@@ -49,7 +112,15 @@ const Single = ({route, navigation}) => {
       </ListItem>
       <ListItem>
         <Icon name="person"></Icon>
-        <Text>{userId}</Text>
+        <Text>{owner.username}</Text>
+      </ListItem>
+      <ListItem>
+        {userLike ? (
+          <Button onPress={removeFavourite}>Remove like</Button>
+        ) : (
+          <Button onPress={createFavourite}>Like</Button>
+        )}
+        <Text>Total likes: {likes.length}</Text>
       </ListItem>
     </Card>
   );
